71c84f92c5128b7a3baeec74d697048cc4d27eef
Arka Chakraborty <rikcarka@gmail.com>
2023-08-28 21:15:22 +0530

    rollback

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,9 +41,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-    @Test
-  public void greetShouldIncludeGreetingMessage() {
-    String someone = "Hello Arka ?";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
 }
